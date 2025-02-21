cb842d150898206c5d293d3385be0f4d0159dee5
Makmont <pa432916@uaeh.edu.mx>
2025-02-21 07:41:44 -0600

    Agregar archivos de proyecto.

---
+++ gasolina/Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace gasolina
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Premium",
+            "Magna",
+            "Diesel"});
+            this.comboBox1.Location = new System.Drawing.Point(173, 68);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 24);
+            this.comboBox1.TabIndex = 0;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "Despachador 1",
+            "Despachador 2"});
+            this.comboBox2.Location = new System.Drawing.Point(173, 107);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 24);
+            this.comboBox2.TabIndex = 1;
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Items.AddRange(new object[] {
+            "DINERO-LITROS",
+            "LITROS-DINERO"});
+            this.comboBox3.Location = new System.Drawing.Point(173, 149);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(121, 24);
+            this.comboBox3.TabIndex = 2;
+            this.comboBox3.SelectedIndexChanged += new System.EventHandler(this.comboBox3_SelectedIndexChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(183, 196);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(169, 243);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(125, 56);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Agregar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(183, 314);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(85, 54);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Informe";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(213, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 16);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "GAS ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(85, 198);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(92, 16);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Litros o Precio";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(478, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.comboBox3);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ gasolina/Form1.cs
@@ -0,0 +1,227 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace gasolina
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        double TotalLitrosVendidos1;
+        double TotalLitrosVendidos2;
+        double TotalPremium1;
+        double TotalMagna1;
+        double TotalDiesel1;
+        double TotalPremium2;
+        double TotalMagna2;
+        double TotalDiesel2;
+        double IngresoGeneralDesp1;
+        double IngresoGeneralDesp2;
+        int ClientesAtendidosDespachador1;
+        int ClientesAtendidosDespachador2;
+        double Pago;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double LitADin = double.Parse(textBox1.Text);
+            double DinALit = double.Parse(textBox1.Text);
+
+            if (comboBox2.Text == "Despachador 1")
+            {
+                if (comboBox3.Text == "LITROS-DINERO" && comboBox1.Text == "Premium")
+                {
+                    Pago = LitADin * 23.40;
+                    TotalPremium1 += LitADin;
+                    TotalLitrosVendidos1 += LitADin;
+                    IngresoGeneralDesp1 += Pago;
+                    ClientesAtendidosDespachador1++;
+                    MessageBox.Show("El cliente deberá pagar: " + Pago);
+                }
+
+                if (comboBox3.Text == "LITROS-DINERO" && comboBox1.Text == "Magna")
+                {
+                    Pago = LitADin * 22.80;
+                    TotalMagna1 += LitADin;
+                    TotalLitrosVendidos1 += LitADin;
+                    IngresoGeneralDesp1 += Pago;
+                    ClientesAtendidosDespachador1++;
+                    MessageBox.Show("El cliente deberá pagar: " + Pago);
+                }
+                if (comboBox3.Text == "LITROS-DINERO" && comboBox1.Text == "Diesel")
+                {
+                    Pago = LitADin * 24.60;
+                    TotalDiesel1 += LitADin;
+                    TotalLitrosVendidos1 += LitADin;
+                    IngresoGeneralDesp1 += Pago;
+                    ClientesAtendidosDespachador1++;
+                    MessageBox.Show("El cliente deberá pagar: " + Pago);
+                }
+
+
+
+                if (comboBox3.Text == "DINERO-LITROS" && comboBox1.Text == "Premium")
+                {
+                    Pago = DinALit;
+                    TotalPremium1 += DinALit / 23.40;
+                    TotalLitrosVendidos1 += TotalPremium1;
+                    IngresoGeneralDesp1 += Pago;
+                    ClientesAtendidosDespachador1++;
+                    MessageBox.Show("El cliente deberá pagar: " + Pago);
+                }
+
+                if (comboBox3.Text == "DINERO-LITROS" && comboBox1.Text == "Magna")
+                {
+                    Pago = DinALit;
+                    TotalMagna1 += DinALit / 23.40;
+                    TotalLitrosVendidos1 += TotalMagna1;
+                    IngresoGeneralDesp1 += Pago;
+                    ClientesAtendidosDespachador1++;
+                    MessageBox.Show("El cliente deberá pagar: " + Pago);
+                }
+
+                if (comboBox3.Text == "DINERO-LITROS" && comboBox1.Text == "Diesel")
+                {
+                    Pago = DinALit;
+                    TotalDiesel1 += DinALit / 23.40;
+                    TotalLitrosVendidos1 += TotalPremium1;
+                    IngresoGeneralDesp1 += Pago;
+                    ClientesAtendidosDespachador1++;
+                    MessageBox.Show("El cliente deberá pagar: " + Pago);
+                }
+            }
+
+            if (comboBox2.Text == "Despachador 2")
+            {
+                if (comboBox3.Text == "LITROS-DINERO" && comboBox1.Text == "Premium")
+                {
+                    Pago = LitADin * 23.40;
+                    TotalPremium2 += LitADin;
+                    TotalLitrosVendidos2 += LitADin;
+                    IngresoGeneralDesp2 += Pago;
+                    ClientesAtendidosDespachador2++;
+                    MessageBox.Show("El cliente deberá pagar: " + Pago);
+                }
+
+                if (comboBox3.Text == "LITROS-DINERO" && comboBox1.Text == "Magna")
+                {
+                    Pago = LitADin * 22.80;
+                    TotalMagna2 += LitADin;
+                    TotalLitrosVendidos2 += LitADin;
+                    IngresoGeneralDesp2 += Pago;
+                    ClientesAtendidosDespachador2++;
+                    MessageBox.Show("El cliente deberá pagar: " + Pago);
+                }
+                if (comboBox3.Text == "LITROS-DINERO" && comboBox1.Text == "Diesel")
+                {
+                    Pago = LitADin * 24.60;
+                    TotalDiesel2 += LitADin;
+                    TotalLitrosVendidos2 += LitADin;
+                    IngresoGeneralDesp2 += Pago;
+                    ClientesAtendidosDespachador2++;
+                    MessageBox.Show("El cliente deberá pagar: " + Pago);
+                }
+
+
+
+                if (comboBox3.Text == "DINERO-LITROS" && comboBox1.Text == "Premium")
+                {
+                    Pago = DinALit;
+                    TotalPremium2 += DinALit / 23.40;
+                    TotalLitrosVendidos2 += TotalPremium2;
+                    IngresoGeneralDesp2 += Pago;
+                    ClientesAtendidosDespachador2++;
+                    MessageBox.Show("El cliente deberá pagar: " + Pago);
+                }
+
+                if (comboBox3.Text == "DINERO-LITROS" && comboBox1.Text == "Magna")
+                {
+                    Pago = DinALit;
+                    TotalMagna2 += DinALit / 23.40;
+                    TotalLitrosVendidos2 += TotalMagna2;
+                    IngresoGeneralDesp2 += Pago;
+                    ClientesAtendidosDespachador2++;
+                    MessageBox.Show("El cliente deberá pagar: " + Pago);
+                }
+
+                if (comboBox3.Text == "DINERO-LITROS" && comboBox1.Text == "Diesel")
+                {
+                    Pago = DinALit;
+                    TotalDiesel2 += DinALit / 23.40;
+                    TotalLitrosVendidos2 += TotalPremium2;
+                    IngresoGeneralDesp2 += Pago;
+                    ClientesAtendidosDespachador2++;
+                    MessageBox.Show("El cliente deberá pagar: " + Pago);
+                }
+            }
+        }
+
+
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            double TotalLitrosFinal = TotalLitrosVendidos1 + TotalLitrosVendidos2;
+            double TotalPremium = TotalPremium1 + TotalPremium2;
+            double TotalMagna = TotalMagna1 + TotalMagna2;
+            double TotalDiessel = TotalDiesel1 + TotalDiesel2;
+            double IngresoGeneral = IngresoGeneralDesp1 + IngresoGeneralDesp2;
+
+            if (ClientesAtendidosDespachador1 > ClientesAtendidosDespachador2)
+            {
+
+                MessageBox.Show("INFORME DE GASOLINERIA"+
+                    "El total de litros vendidos por el despachador 1 es: " + TotalLitrosVendidos1
+                    + "\nEl total de litros vendidos por el despachador 2 es: " + TotalLitrosVendidos2
+                    + "\nEl despachador 1 vendio: " + TotalPremium1 + " litros de Premium"
+                    + "\nEl despachador 2 vendio: " + TotalPremium2 + " litros de Premium"
+                    + "\nEl despachador 1 vendio: " + TotalMagna1 + " litros de Magna"
+                    + "\nEl despachador 2 vendio: " + TotalMagna2 + " litros de Magna"
+                    + "\nEl despachador 1 vendio: " + TotalDiesel1 + " litros de Diesel"
+                    + "\nEl despachador 2 vendio: " + TotalDiesel2 + " litros de Diesel"
+                    + "\nEl despachador 1 atendio:" + ClientesAtendidosDespachador1 + " clientes"
+                    + "\nEl despachador 2 atendio:" + ClientesAtendidosDespachador2 + " clientes"
+                    + "\nEl ingreso general es de: " + IngresoGeneral+"\n"
+                    + "El despachador 1 obtuvo más venta");
+            }
+            else
+            {
+                MessageBox.Show("INFORME DE GASOLINERIA"+
+                    "El total de litros vendidos por el despachador 1 es: " + TotalLitrosVendidos1
+                    + "\nEl total de litros vendidos por el despachador 2 es: " + TotalLitrosVendidos2
+                    + "\nEl despachador 1 vendio: " + TotalPremium1 + " litros de  Premium"
+                    + "\nEl despachador 2 vendio: " + TotalPremium2 + " litros de  Premium"
+                    + "\nEl despachador 1 vendio: " + TotalMagna1 + " litros de Magna"
+                    + "\nEl despachador 2 vendio: " + TotalMagna2 + " litros de  Magna"
+                    + "\nEl despachador 1 vendio: " + TotalDiesel1 + " litros de  Diesel"
+                    + "\nEl despachador 2 vendio: " + TotalDiesel2 + " litros de  Diesel"
+                    + "\nEl despachador 1 atendio:" + ClientesAtendidosDespachador1 + " clientes"
+                    + "\nEl despachador 2 atendio:" + ClientesAtendidosDespachador2 + " clientes"
+                    + "\nEl despachador 1 vendio:" + IngresoGeneralDesp1
+                    + "\nEl despachador 2 vendio:" + IngresoGeneralDesp2
+                    + "\nEl ingreso general es de: " + IngresoGeneral
+                    + "\nEl despachador 2 obtuvo más venta");
+
+            }
+        }
+
+        private void comboBox3_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
